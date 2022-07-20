efef8849673921b0895191b09e947f933bf26d5d
ShirleneSousa <103758882+ShirleneSousa@users.noreply.github.com>
2022-07-20 00:49:47 -0300

    Create 1013.c

---
+++ Iniciantes/1013.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+int a,b,c,ab,maior;
+
+scanf("%d %d %d", &a, &b, &c);
+
+ab = (a+b+abs(a-b))/2;
+
+maior = (ab+c+abs(ab-c))/2;
+
+printf("%d eh o maior\n", maior);
+
+return 0;
+}
