f128027365a5dc47ea62be8a5f5937f8da727044
ShirleneSousa <103758882+ShirleneSousa@users.noreply.github.com>
2023-02-03 19:41:29 -0300

    Create 1072.c

---
+++ Iniciantes/1072.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n, i = 0, x, in = 0, out = 0;
+    scanf("%d", &n);
+    while (i < n)
+    {
+        scanf("%d", &x);
+        if (x >= 10 && x <= 20)
+        {
+            in++;
+        }
+        else
+        {
+            out++;
+        }
+        i++;
+    }
+    printf("%d in\n%d out\n", in, out);
+    return 0;
+}
