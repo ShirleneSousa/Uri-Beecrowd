ad47255051f6ee5ec7a3a19769f35a5eadaffb49
ShirleneSousa <103758882+ShirleneSousa@users.noreply.github.com>
2023-02-03 19:40:34 -0300

    Create 1071.c

---
+++ Iniciantes/1071.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a, b, i, soma = 0;
+    scanf("%d %d", &a, &b);
+    if (a < b)
+    {
+        for (i = a + 1; i < b; i++)
+        {
+            if (i % 2 != 0)
+            {
+                soma += i;
+            }
+        }
+    }
+        else
+        {
+            for (i = b + 1; i < a; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    soma += i;
+                }
+            }
+        }
+        printf("%d\n", soma);
+        return 0;
+    }
+    
